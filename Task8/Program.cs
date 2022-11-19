65f3cb48a83be3f93b366125d9b8b42e14790077
Roman <roman.tsomka@yndex.ru>
2022-11-19 17:24:46 +1000

    Добавил задачу 10 из второго урока

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -3,16 +3,13 @@ Console.Write("Введите число:");
 int N = int.Parse(Console.ReadLine());
 int i = 1;  
    
-while (true)
+while (i < N+1)
 {
 	if (i % 2 == 0)
 	{
 		Console.Write(i + " ");
-		
 	}
-	if (i >= N)
-	{
-		break;
-	}
-	i++;
+	
+i++;
+	
 }
---
+++ Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int num_1, num_2;
+Console.WriteLine("Введите число: ");
+num_1 = Convert.ToInt32(Console.ReadLine());
+num_2 = (num_1 / 10) % 10;
+Console.WriteLine(num_2);
